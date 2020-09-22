d0a1c3302c3ef879137ac55179a13e6d55ac26f1
pedrocaximiliano <pedro_caximiliano@hotmail.com>
2020-09-21 22:43:47 -0300

    first commit

---
+++ src/translate/languages/en.js
@@ -0,0 +1,19 @@
+const messages = {
+  en: {
+    translations: {
+      titles: {
+        app: "Using i18n for applications: {{name}}",
+      },
+      messages: {
+        itWorks: "welcome to our website",
+        smallText: "choose a language",
+        selectLanguage: "select language",
+      },
+      buttons: {
+        save: "Save data",
+      },
+    },
+  },
+};
+
+export { messages };
---
+++ src/translate/languages/pt.js
@@ -0,0 +1,19 @@
+const messages = {
+  pt: {
+    translations: {
+      titles: {
+        app: "Uso do i18n para aplicações : {{name}}",
+      },
+      messages: {
+        itWorks: "Seja bem vindo ao nosso site",
+        smallText: "escolha um idioma",
+        selectLanguage: "selecione um idioma",
+      },
+      buttons: {
+        save: "Salvar dados",
+      },
+    },
+  },
+};
+
+export { messages };
